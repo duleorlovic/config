1596c70d9f54c984f3378ddbefc7f76a9632c062
Dusan Orlovic <dule@hey.com>
2023-02-28 18:02:03 +0100

    Add seat statuses

---
+++ loginctl_seats/seat_statuses.sh
@@ -0,0 +1,7 @@
+set -x # Show command being executed
+
+sudo loginctl seat-status seat0 > ~/config/loginctl_seats/seat0.seat-status.txt
+sudo loginctl seat-status seat1 > ~/config/loginctl_seats/seat1.seat-status.txt
+sudo loginctl seat-status seat2 > ~/config/loginctl_seats/seat2.seat-status.txt
+sudo loginctl seat-status seat3 > ~/config/loginctl_seats/seat3.seat-status.txt
+
