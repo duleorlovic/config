d73adaeaa8205c4caf3afc0e3be0c70f4a34d73a
Dusan Orlovic <dule@hey.com>
2023-10-14 18:41:10 +0200

    update

--- my_bashrc.sh
+++ my_bashrc.sh
@@ -213,7 +213,8 @@ export HISTFILESIZE=1000000
 export HISTSIZE=1000000
 
 # access sibling folders
-export CDPATH="..:"
 
 # pip installed ansible command should be accessible
 export PATH=~/.local/bin:$PATH
+
+export CDPATH=".:.."
