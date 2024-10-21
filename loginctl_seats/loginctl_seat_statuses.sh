0bb35ff09f53ded1788edb4cf85aeaa9a6062b74
Dusan Orlovic <dule@trk.in.rs>
2024-10-21 16:38:12 +0200

    Add loginctl

--- loginctl_seats/loginctl_seat_statuses.sh
+++ loginctl_seats/loginctl_seat_statuses.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 set -x
 # Determine numbers of PCI graphic cards, look at first two digits
 #
@@ -22,26 +23,43 @@ set -x
 
 # SEAT1 top graphic card
 # sudo loginctl attach seat1 /sys/devices/pci0000:00/0000:00:03.1/0000:08:00.0/drm/*
+# sudo loginctl attach seat1 /sys/devices/pci0000:00/0000:00:03.1/0000:08:00.0/graphics/*
 # sudo loginctl attach seat1 /sys/devices/pci0000:00/0000:00:03.1/0000:08:00.1/sound/*
 # two usb on front above leds and power button
-# sudo loginctl attach seat1 /sys/devices/pci0000:00/0000:00:01.2/0000:02:00.0/usb1/1-10
+# sudo loginctl attach seat1 /sys/devices/pci0000:00/0000:00:01.2/*
 #
 # SEAT2 bottom graphic card
 # sudo loginctl attach seat2 /sys/devices/pci0000:00/0000:00:01.2/0000:02:00.2/0000:03:04.0/0000:05:00.0/drm/*
+# sudo loginctl attach seat2 /sys/devices/pci0000:00/0000:00:01.2/0000:02:00.2/0000:03:04.0/0000:05:00.0/graphics/*
 # sudo loginctl attach seat2 /sys/devices/pci0000:00/0000:00:01.2/0000:02:00.2/0000:03:04.0/0000:05:00.1/sound/*
 # sudo loginctl attach seat2 /sys/devices/pci0000:00/0000:00:08.1/0000:0a:00.3/usb3
 #
 # git init .
 # sudo ls
-# ./seat_statuses.sh
-# inspect all changes and determine the ports, card1 on restart could be card2
+# ./login_seat_statuses.sh
+# inspect all changes and determine the ports, card1 on restart could be card2,
+# and usb1 could be usb2, so you might need to run ./login_seat_statuses.sh
+# multiple times to attach all those variations
 # git add .
 
-# Note that when seat_statuses.sh is triggered, the devices should be already
+# Note that when login_seat_statuses.sh is triggered, the devices should be already
 # connected so for both old usb and usb3 protocol, you need to run when old
 # device is connected and again when usb3 device is connected in the same ports
 # When monitor is turned off than graphics/fb2 is not present
-# On restart card1 could become card1
+#
+# udevadm monitor --environment --udev
+# Udevadm monitor will show you at which path usb device is connected so use
+# ports that has different prefix, example, instead of full path
+# sudo loginctl attach seat2 /sys/devices/pci0000:00/0000:00:08.1/0000:0a:00.3/usb3
+# you can have
+# sudo loginctl attach seat2 /sys/devices/pci0000:00/0000:00:08.1/*
+
+# at the end, run
+# sudo loginctl flush-devices
+# ./attach_seats.sh
+# Power off (sudo reboot does not work in some cases)
+# power on
+# repeat ./attach_seats.sh if new device is connected or usb number changed
 
 sudo loginctl seat-status seat0 > ~/config/loginctl_seats/seat0.seat-status.txt
 sudo loginctl seat-status seat1 > ~/config/loginctl_seats/seat1.seat-status.txt || true
